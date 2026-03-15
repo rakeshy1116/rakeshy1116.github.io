6adc95e0d16319e52fd2b26cdf7a9834fa218fcd
Rakesh Chowdary Yarlagadda <yrakeshchowdary1116@gmail.com>
2026-03-14 23:25:25 -0500

    fix: heading sizes, profile image, and skill bar layout

---
+++ build/static/media/about.85e05836ff81b9acc910.md
@@ -0,0 +1,19 @@
+
+# Intro
+
+Hello, I'm Rakesh Chowdary. I am a Data Engineer III at <a href="https://tech.walmart.com/">Walmart Global Tech</a> and an M.S. Computer Science graduate (GPA 4.0) from the <a href="https://www.colorado.edu/">University of Colorado Boulder</a>. I was previously a Data Engineer at <a href="https://corporate.target.com/">Target Corporation</a>. I am particularly interested in projects with high social impact. If you think I can be helpful to you or your cause and would like to meet, please feel free to get in touch.
+
+# I like
+
+- [Books](https://www.goodreads.com/user/show/170615969?ref=nav_profile_l) ([Shoe Dog](https://www.goodreads.com/book/show/27220736-shoe-dog), [Designing Data-Intensive Applications](https://www.goodreads.com/book/show/23463279-designing-data-intensive-applications?ref=nav_sb_ss_1_11), [Fundamentals of Data Engineering](https://www.goodreads.com/book/show/61218623-fundamentals-of-data-engineering?ref=nav_sb_ss_1_16))
+- Cricket
+- Drama TV Shows
+- Summer
+- Podcasts ([Lex Fridman](https://lexfridman.com/podcast/), [Planet Money](https://www.npr.org/sections/money/))
+- [Good design](/)
+
+# I dream of
+
+- building systems that scale to millions and matter to people.
+- working on problems at the intersection of data, infrastructure, and real-world impact.
+- always learning something new.
--- src/components/Resume/Skills/SkillBar.js
+++ src/components/Resume/Skills/SkillBar.js
@@ -4,23 +4,24 @@ import PropTypes from 'prop-types';
 const SkillBar = ({ data, categories }) => {
   const { category, competency, title } = data;
 
-  // TODO: Consider averaging colors
-  const titleStyle = {
-    background: categories
-      .filter((cat) => category.includes(cat.name))
-      .map((cat) => cat.color)[0],
-  };
+  const color = categories
+    .filter((cat) => category.includes(cat.name))
+    .map((cat) => cat.color)[0];
 
-  const barStyle = {
-    ...titleStyle,
-    width: `${String(Math.min(100, Math.max((competency / 5.0) * 100.0, 0)))}%`,
-  };
+  const fillWidth = `${Math.min(100, Math.max((competency / 5.0) * 100.0, 0))}%`;
 
   return (
-    <div className="skillbar clearfix">
-      <div className="skillbar-title" style={titleStyle}><span>{title}</span></div>
-      <div className="skillbar-bar" style={barStyle} />
-      <div className="skill-bar-percent">{competency} / 5</div>
+    <div className="skillbar-modern">
+      <div className="skillbar-modern__header">
+        <span className="skillbar-modern__label">{title}</span>
+        <span className="skillbar-modern__percent">{competency} / 5</span>
+      </div>
+      <div className="skillbar-modern__track">
+        <div
+          className="skillbar-modern__fill"
+          style={{ width: fillWidth, background: color }}
+        />
+      </div>
     </div>
   );
 };
